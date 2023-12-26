0d77d0b334e96328c7476842e827668f610b99d9
justbeyourselfandenjoy <justbeyourselfandenjoy@email.com>
2023-12-26 12:35:21 +0200

    Tests are added

--- sorting/bubble/bubble_test.go
+++ sorting/bubble/bubble_test.go
@@ -1,6 +1,31 @@
 package bubble
 
-/*
-func TestmakeRandomSlice(t *testing.T) {
+import (
+	"slices"
+	"testing"
+)
+
+var MakeRandomSlice = makeRandomSlice
+var BubbleSort = bubbleSort
+
+func TestMakeRandomSlice(t *testing.T) {
+	slice := makeRandomSlice(100, 200)
+	if len(slice) != 100 {
+		t.Fatalf("Wrong slice size: %v instead of %v", len(slice), 100)
+	}
+	v := slices.Max(slice)
+	if v > 200 {
+		t.Fatalf("Wrong slice max value: %v", v)
+	}
 }
-*/
+
+func TestBubbleSort(t *testing.T) {
+	slice_origin_1 := makeRandomSlice(100, 200)
+	slice_origin_2 := make([]int, 100)
+	copy(slice_origin_2, slice_origin_1)
+	bubbleSort(slice_origin_1)
+	slices.Sort(slice_origin_2)
+	if slices.Compare(slice_origin_1, slice_origin_2) != 0 {
+		t.Fatalf("Broken sorting detected: %v %v", slice_origin_1, slice_origin_2)
+	}
+}
--- sorting/quicksort/quick_sort.go
+++ sorting/quicksort/quick_sort.go
@@ -1,5 +1,1 @@
 package quicksort
-
-func makeRandomSlice2(numItems, max int) []int {
-	return make([]int, 0)
-}
