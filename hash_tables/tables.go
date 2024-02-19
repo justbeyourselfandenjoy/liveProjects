3a62de99b0e1b79404ac726af927e24b5365d25e
justbeyourselfandenjoy <justbeyourselfandenjoy@email.com>
2024-02-19 09:59:52 +0200

    removing from linear probing

--- hash_tables/tables.go
+++ hash_tables/tables.go
@@ -3,9 +3,11 @@ package main
 import (
 	"justbeyourselfandenjoy/hash_tables/chaining"
 	linearprobing "justbeyourselfandenjoy/hash_tables/linear_probing"
+	linearremoving "justbeyourselfandenjoy/hash_tables/linear_removing"
 )
 
 func main() {
 	chaining.ChainingRun()
 	linearprobing.LinearProbingRun()
+	linearremoving.LinearProbingRemovingRun()
 }
