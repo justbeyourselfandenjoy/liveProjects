739009a128a83c853550b7f51ebdd74b3d37a144
justbeyourselfandenjoy <justbeyourselfandenjoy@email.com>
2024-04-19 10:05:33 +0300

    getenv stub + test

---
+++ github sign-in/milestone-3/github_callback_handler.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+func githubCallbackHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println("githubCallbackHandler called")
+
+	reqContext := req.Context()
+
+	//checking OAuthState == state
+	cookie, err := req.Cookie(oauthStateCookie)
+	stateFromURI := req.URL.Query().Get("state")
+
+	if err != nil || stateFromURI != cookie.Value {
+		log.Println("State tokens don't match. Ignoring callback.")
+		http.Error(w, "Invalid callback request", http.StatusBadRequest)
+		return
+	}
+
+	codeFromURI := req.URL.Query().Get("code")
+
+	reqContext = context.WithValue(reqContext, oauth2.HTTPClient, oauthHttpClient)
+
+	token, err := oauthConf.Exchange(reqContext, codeFromURI)
+	if err != nil {
+		log.Println("oAuth exchange error: ", err)
+		http.Error(w, "Error logging in.", http.StatusInternalServerError)
+		return
+	}
+
+	session, err := createSession(reqContext, token.AccessToken)
+	if err != nil {
+		log.Println("Error creating session: ", err)
+		http.Error(w, "Error creating session", http.StatusInternalServerError)
+		return
+	}
+
+	setCookie(w, sessionCookie, session.ID, sessionCookieMaxAge)
+	setCookie(w, oauthStateCookie, "", -1)
+	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
+}
